1c2e195b451996322b34f1cd4c2670447e0bde5a
Lez Cheung <34307258+Lezzkafka@users.noreply.github.com>
2019-02-11 10:30:31 +1000

    Added Data @Block struct

--- Basic_blocks/hash-gen-verify.go
+++ Basic_blocks/hash-gen-verify.go
@@ -10,13 +10,13 @@ import (
 type Block struct {
 	Index     int
 	Timestamp string
+	Data      string
 	PrevHash  string
 	Hash      string
 }
 
 func hashfunction(block Block) string {
-
-	header := string(block.Index) + block.PrevHash + block.Timestamp
+	header := string(block.Index) + block.Data + block.PrevHash + block.Timestamp
 	hash_byte := sha256.Sum256([]byte(header))
 	return hex.EncodeToString(hash_byte[:])
 }
@@ -54,24 +54,24 @@ func blocksvaildation(prevblock, newblock Block) bool {
 func main() {
 	t0 := time.Now().String()
 
-	var b0 = Block{0, t0, "_", "_"}
+	var b0 = Block{0, t0, "Let there be light", "_", "_"}
 	fmt.Println(b0)
 
 	time.Sleep(time.Second)
 
 	b1 := Block(genblock(b0))
-	fmt.Println(b1)
-	fmt.Println(blocksvaildation(b0, b1))
+	fmt.Printf("Block %v, \n \t Time:%v; \n \t Hash: %v;\n", b1.Index, b1.Timestamp, b1.Hash)
+	fmt.Printf("\t Check: %v.\n", blocksvaildation(b0, b1))
 
 	time.Sleep(time.Second)
 
 	b2 := Block(genblock(b1))
-	fmt.Println(b2)
-	fmt.Println(blocksvaildation(b1, b2))
+	fmt.Printf("Block %v, \n \t Time:%v; \n \t Hash: %v;\n", b2.Index, b2.Timestamp, b2.Hash)
+	fmt.Printf("\t Check: %v.\n", blocksvaildation(b1, b2))
 
 	time.Sleep(time.Second)
 
 	b3 := Block(genblock(b2))
-	fmt.Println(b3)
-	fmt.Println(blocksvaildation(b2, b3))
+	fmt.Printf("Block %v, \n \t Time:%v; \n \t Hash: %v;\n", b3.Index, b3.Timestamp, b3.Hash)
+	fmt.Printf("\t Check: %v.\n", blocksvaildation(b2, b3))
 }
